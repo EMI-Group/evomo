cfbf4bab1432916270b56ddbfc4608b0ca06e36d
Zhenyu2Liang <1370096263@qq.com>
2025-03-25 23:21:14 +0800

    delete legacy

--- legacy/algorithms/__init__.py
+++
@@ -1,7 +0,0 @@
-from .moead_origin import MOEAD
-from .tensormoead import TensorMOEAD
-from .hype_origin import HypE
-from .tensorhype import TensorHypE
-from .nsga3_origin import NSGA3
-from .tensornsga3 import TensorNSGA3
-from .mo_random import MORandom
